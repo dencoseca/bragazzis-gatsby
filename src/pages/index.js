6ac3850c18777368d0d636779ac9271410e5b6e8
Leon Brown <leon.brown71@gmail.com>
2021-03-17 10:41:54 +0000

    add main element hidden on page load to prevent flashing

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,6 +34,9 @@ function Home({ data }) {
 
   // Set first when component loads
   useEffect(() => {
+    // Prevent flashing
+    document.querySelector("main").classList.add("visible")
+
     setDimensions({
       height: window.innerHeight,
       width: window.innerWidth,
@@ -68,37 +71,39 @@ function Home({ data }) {
   return (
     <>
       <SEO title="Home" />
-      <Cover openingHours={openingHours} dimensions={dimensions} />
-      <section className="statement">
-        <span>Roam freely and find inspiration...</span>
-        <span>or that obscure pasta shape that you've</span>
-        <span>been looking for</span>
-      </section>
-      <FloatingItems dimensions={dimensions} />
-      <FullWidthBanner dimensions={dimensions} />
-      <section className="covid-notice">
-        <h3>Covid Restrictions</h3>
-        <p>
-          We want to reassure you that we are putting all measures in place to
-          keep you and our team safe so that you can confidently enjoy your
-          visit.
-        </p>
-        <p>
-          We are following all government guidelines and working on practice
-          measures within the shop. We have completed necessary risk assessments
-          and all procedures which will be constantly reviewed and updated where
-          necessary in line with government guidelines.
-        </p>
-        <p>
-          We ask you to please use common sense to play your part keeping
-          everyone safe. In return we will be doing the same for you.
-        </p>
-        <p>
-          Due to the constantly changing nature of this guidance, we will be
-          continuously reviewing our procedures related to capacity.
-        </p>
-      </section>
-      <Footer />
+      <main>
+        <Cover openingHours={openingHours} dimensions={dimensions} />
+        <section className="statement">
+          <span>Roam freely and find inspiration...</span>
+          <span>or that obscure pasta shape that you've</span>
+          <span>been looking for</span>
+        </section>
+        <FloatingItems dimensions={dimensions} />
+        <FullWidthBanner dimensions={dimensions} />
+        <section className="covid-notice">
+          <h3>Covid Restrictions</h3>
+          <p>
+            We want to reassure you that we are putting all measures in place to
+            keep you and our team safe so that you can confidently enjoy your
+            visit.
+          </p>
+          <p>
+            We are following all government guidelines and working on practice
+            measures within the shop. We have completed necessary risk
+            assessments and all procedures which will be constantly reviewed and
+            updated where necessary in line with government guidelines.
+          </p>
+          <p>
+            We ask you to please use common sense to play your part keeping
+            everyone safe. In return we will be doing the same for you.
+          </p>
+          <p>
+            Due to the constantly changing nature of this guidance, we will be
+            continuously reviewing our procedures related to capacity.
+          </p>
+        </section>
+        <Footer />
+      </main>
     </>
   )
 }
