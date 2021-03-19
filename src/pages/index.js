d73370c23ba4ef18b0808676430c30089d5412b0
Leon Brown <leon.brown71@gmail.com>
2021-03-19 12:38:50 +0000

    fix whitespace in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,7 +80,7 @@ function Home({ data }) {
         <FloatingItems dimensions={dimensions} />
         <FullWidthBanner dimensions={dimensions} />
         <section className="covid-notice">
-        <h3 className="text--display">Covid Restrictions</h3>
+          <h3 className="text--display">Covid Restrictions</h3>
           <p className="text--md">
             We want to reassure you that we are putting all measures in place to
             keep you and our team safe so that you can confidently enjoy your
