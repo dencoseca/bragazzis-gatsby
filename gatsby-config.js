c5f947ac5d27db4203b77fd716c71ada8ebcce5c
Leon Brown <leon.brown71@gmail.com>
2021-03-31 15:12:53 +0100

    add robots.txt and configure to prevent Netlify preview build being crawled

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,21 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://www.example.com",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === "production"
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   siteMetadata: {
     title: `Bragazzis`,
     titleTemplate: `Bragazzi's | %s`,
     description: `Purveyors of quality Italian goods.`,
-    siteUrl: `https://www.bragazzis.co.uk`,
+    siteUrl,
     author: `Leon Brown`,
     image: `src/images/big-b-square-icon.png`,
   },
@@ -48,5 +57,26 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: `*` }],
+          },
+          "branch-deploy": {
+            policy: [{ userAgent: `*`, disallow: [`/`] }],
+            sitemap: null,
+            host: null,
+          },
+          "deploy-preview": {
+            policy: [{ userAgent: `*`, disallow: [`/`] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
   ],
 }
