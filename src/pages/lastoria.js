99430609e5f1c7fa275145dc6c9c21ca20156db0
Leon Brown <leon.brown71@gmail.com>
2021-04-07 15:27:29 +0100

    restyle lastoria

--- src/pages/lastoria.js
+++ src/pages/lastoria.js
@@ -22,61 +22,33 @@ function LaStoria({ location }) {
               alt="plane ticket"
             />
           </div>
-          <div className="lastoria__story">
-            <div className="slide left">
-              <StaticImage
-                className="slide__image"
-                src="../images/cafe-view.jpg"
-                alt="cafe view"
-              />
-              <div className="slide__text text--md">
-                <p>
-                  Bragazzi’s opened in Sheffield in 2003 and is owned by Matteo
-                  Bragazzi. Bragazzi’s is an outlier and safe haven for people
-                  who enjoy the “qualcosa in più“.
-                </p>
-              </div>
-              <div className="slide__blank-space"></div>
-            </div>
-            <div className="slide right">
-              <StaticImage
-                className="slide__image"
-                src="../images/cafe-view.jpg"
-                alt="cafe view"
-              />
-              <div className="slide__text text--md">
-                <p>
-                  Matteo has a brother, Dino, they often holiday together. In
-                  Rome one evening, enjoying a Shakerato, Matteo’s mind drifted.
-                  Sorry to see him this way, Dino started up a monologue on
-                  their family history of Italian dining in London. Their father
-                  had come over, like so many others, and made a business of
-                  selling food.
-                </p>
-              </div>
-              <div className="slide__blank-space"></div>
-            </div>
-            <div className="slide left">
-              <StaticImage
-                className="slide__image"
-                src="../images/cafe-view.jpg"
-                alt="cafe view"
-              />
-              <div className="slide__text text--md">
-                <p>
-                  As Dino reached a point about the Corradi brothers, Matteo
-                  recognised his fate as the same. And so, the bet was placed
-                  over a plastic table, outside a bar in Fiano Romano on that
-                  hot evening in 2002. They did a big shop with help from Zia
-                  Maria and floated it to England, ready for the cafe to come.
-                </p>
-              </div>
-              <div className="slide__blank-space"></div>
-            </div>
-          </div>
           <div className="lastoria__statement text--display">
             <div>If you build it, they will come...</div>
             <div>and drink espresso</div>
+          </div>
+          <div className="lastoria__story">
+            <StaticImage src="../images/cafe-view.jpg" alt="A busy cafe" />
+            <p className="text--md">
+              Bragazzi’s opened in Sheffield in 2003 and is owned by Matteo
+              Bragazzi. Bragazzi’s is an outlier and safe haven for people who
+              enjoy the “qualcosa in più“.
+            </p>
+            <StaticImage src="../images/cafe-view.jpg" alt="A busy cafe" />
+            <p className="text--md">
+              Matteo has a brother, Dino, they often holiday together. In Rome
+              one evening, enjoying a Shakerato, Matteo’s mind drifted. Sorry to
+              see him this way, Dino started up a monologue on their family
+              history of Italian dining in London. Their father had come over,
+              like so many others, and made a business of selling food.
+            </p>
+            <StaticImage src="../images/early-days.jpg" alt="A busy cafe" />
+            <p className="text--md">
+              As Dino reached a point about the Corradi brothers, Matteo
+              recognised his fate as the same. And so, the bet was placed over a
+              plastic table, outside a bar in Fiano Romano on that hot evening
+              in 2002. They did a big shop with help from Zia Maria and floated
+              it to England, ready for the cafe to come.
+            </p>
           </div>
         </div>
       </Layout>
