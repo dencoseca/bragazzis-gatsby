91bb68b127d2fbd85cc51b5d7f263c922224530e
Leon Brown <leon.brown71@gmail.com>
2021-04-08 17:57:47 +0100

    create size classes for image and image--with-caption class

--- src/pages/ilgiorno.js
+++ src/pages/ilgiorno.js
@@ -9,54 +9,72 @@ function Ilgiorno({ location }) {
     <Layout pageTitle="Il Giorno" location={location}>
       <div className="ilgiorno__title text--page-title">IL GIORNO</div>
       <div className="ilgiorno__gallery">
-        <div className="text text--display">Aperto</div>
+        <div className="image--with-caption">
+          <StaticImage
+            className="image image--60"
+            src="../images/cafe-view.jpg"
+            alt="cafe view"
+          />
+          <div className="text text--display">Aperto</div>
+        </div>
         <StaticImage
-          className="image"
+          className="image image--50"
           src="../images/cafe-view.jpg"
           alt="cafe view"
         />
         <StaticImage
-          className="image"
+          className="image image--50"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <div className="image--with-caption row-reverse">
+          <StaticImage
+            className="image image--60"
+            src="../images/cafe-view.jpg"
+            alt="cafe view"
+          />
+          <div className="text text--display">Aperto</div>
+        </div>
+        <StaticImage
+          className="image image--60"
           src="../images/cafe-view.jpg"
           alt="cafe view"
         />
         <StaticImage
-          className="image"
+          className="image image--50"
           src="../images/cafe-view.jpg"
           alt="cafe view"
         />
         <StaticImage
-          className="image"
+          className="image image--70"
           src="../images/cafe-view.jpg"
           alt="cafe view"
         />
         <StaticImage
-          className="image"
+          className="image image--50"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <div className="image--with-caption">
+          <StaticImage
+            className="image image--60"
+            src="../images/cafe-view.jpg"
+            alt="cafe view"
+          />
+          <div className="text text--display">Aperto</div>
+        </div>
+        <StaticImage
+          className="image image--50"
           src="../images/cafe-view.jpg"
           alt="cafe view"
         />
         <StaticImage
-          className="image"
+          className="image image--60"
           src="../images/cafe-view.jpg"
           alt="cafe view"
         />
         <StaticImage
-          className="image"
-          src="../images/cafe-view.jpg"
-          alt="cafe view"
-        />
-        <StaticImage
-          className="image"
-          src="../images/cafe-view.jpg"
-          alt="cafe view"
-        />
-        <StaticImage
-          className="image"
-          src="../images/cafe-view.jpg"
-          alt="cafe view"
-        />
-        <StaticImage
-          className="image"
+          className="image image--70"
           src="../images/cafe-view.jpg"
           alt="cafe view"
         />
