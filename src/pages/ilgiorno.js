2b966bed70e843d7537beab08f031d50ae070112
Leon Brown <leon.brown71@gmail.com>
2021-04-08 18:27:51 +0100

    add image files for il giorno and layout the morning group

--- src/pages/ilgiorno.js
+++ src/pages/ilgiorno.js
@@ -12,70 +12,69 @@ function Ilgiorno({ location }) {
         <div className="image--with-caption">
           <StaticImage
             className="image image--60"
-            src="../images/cafe-view.jpg"
+            src="../images/aperto.jpg"
             alt="cafe view"
           />
           <div className="text text--display">Aperto</div>
         </div>
         <StaticImage
           className="image image--50"
-          src="../images/cafe-view.jpg"
+          src="../images/window-clouds.jpg"
           alt="cafe view"
         />
         <StaticImage
           className="image image--50"
-          src="../images/cafe-view.jpg"
-          alt="cafe view"
-        />
-        <div className="image--with-caption row-reverse">
-          <StaticImage
-            className="image image--60"
-            src="../images/cafe-view.jpg"
-            alt="cafe view"
-          />
-          <div className="text text--display">Aperto</div>
-        </div>
-        <StaticImage
-          className="image image--60"
-          src="../images/cafe-view.jpg"
+          src="../images/stacked-chairs-morning.jpg"
           alt="cafe view"
         />
         <StaticImage
           className="image image--50"
-          src="../images/cafe-view.jpg"
-          alt="cafe view"
-        />
-        <StaticImage
-          className="image image--70"
-          src="../images/cafe-view.jpg"
+          src="../images/sandwich-prep-duo.jpg"
           alt="cafe view"
         />
         <StaticImage
           className="image image--50"
-          src="../images/cafe-view.jpg"
+          src="../images/sandwich-board-plan.jpg"
           alt="cafe view"
         />
-        <div className="image--with-caption">
-          <StaticImage
-            className="image image--60"
-            src="../images/cafe-view.jpg"
-            alt="cafe view"
-          />
-          <div className="text text--display">Aperto</div>
-        </div>
         <StaticImage
           className="image image--50"
-          src="../images/cafe-view.jpg"
+          src="../images/olive-oil-bread.jpg"
           alt="cafe view"
         />
         <StaticImage
-          className="image image--60"
-          src="../images/cafe-view.jpg"
+          className="image image--50"
+          src="../images/sanger-mix.jpg"
           alt="cafe view"
         />
         <StaticImage
-          className="image image--70"
-          src="../images/cafe-view.jpg"
+          className="image image--50"
+          src="../images/rocket-sangers.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image image--50"
+          src="../images/coppa-focaccia.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image image--50"
+          src="../images/sangers-in-baskets.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image image--50"
+          src="../images/prep-cleanup.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image image--50"
+          src="../images/cake-board.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image image--50"
+          src="../images/writing-cake-labels.jpg"
           alt="cafe view"
         />
       </div>
