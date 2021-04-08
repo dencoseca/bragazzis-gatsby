95b67140128e42e41eeb8272e443b3631593eba2
Leon Brown <leon.brown71@gmail.com>
2021-04-08 16:22:53 +0100

    create placement scheme for image gallery in sass

--- src/pages/ilgiorno.js
+++ src/pages/ilgiorno.js
@@ -7,11 +7,60 @@ import Layout from "../components/Layout"
 function Ilgiorno({ location }) {
   return (
     <Layout pageTitle="Il Giorno" location={location}>
-        <div className="ilgiorno__title text--page-title">IL GIORNO</div>
-        <div className="ilgiorno__gallery">
-          <div className="gallery__text text--display">Aperto</div>
-          <StaticImage src="../images/cafe-view.jpg" alt="cafe view" />
-        </div>
+      <div className="ilgiorno__title text--page-title">IL GIORNO</div>
+      <div className="ilgiorno__gallery">
+        <div className="text text--display">Aperto</div>
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+        <StaticImage
+          className="image"
+          src="../images/cafe-view.jpg"
+          alt="cafe view"
+        />
+      </div>
     </Layout>
   )
 }
