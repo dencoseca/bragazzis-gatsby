8ceec720431113f59da0668f0411e5547e9e942d
Leon Brown <leon.brown71@gmail.com>
2021-04-11 16:49:53 +0100

    animate menu links

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,17 +5,44 @@ import { Link } from "gatsby"
 // Animations
 const smoothTransition = {
   duration: 0.6,
-  ease: [0.43, 0.13, 0.13, 0.96],
+  ease: [0.13, 0.43, 0.45, 0.96],
 }
 
 const menuVariants = {
   closed: {
     translateX: "-100%",
-    opacity: 0,
+    // opacity: 0,
+    transition: {
+      ...smoothTransition,
+    },
   },
   open: {
     translateX: "0%",
+    // opacity: 1,
+    transition: {
+      ...smoothTransition,
+      staggerChildren: 0.2,
+      delayChildren: 0.3,
+    },
+  },
+}
+
+const linkVariants = {
+  closed: {
+    opacity: 0,
+    translateX: -200,
+    transition: {
+      ...smoothTransition,
+      duration: 0.4,
+    },
+  },
+  open: {
     opacity: 1,
+    translateX: 0,
+    transition: {
+      ...smoothTransition,
+      duration: 0.9,
+    },
   },
 }
 
@@ -27,17 +54,22 @@ function Menu() {
       animate="open"
       exit="closed"
       variants={menuVariants}
-      transition={smoothTransition}
     >
-      <Link className="menu__link text--menu-link" to="/">
-        Home
-      </Link>
-      <Link className="menu__link text--menu-link" to="/lastoria">
-        La Storia
-      </Link>
-      <Link className="menu__link text--menu-link" to="/ilgiorno">
-        Il Giorno
-      </Link>
+      <motion.div className="menu__link-wrapper" variants={linkVariants}>
+        <Link className="menu__link text--menu-link" to="/">
+          Home
+        </Link>
+      </motion.div>
+      <motion.div className="menu__link-wrapper" variants={linkVariants}>
+        <Link className="menu__link text--menu-link" to="/lastoria">
+          La Storia
+        </Link>
+      </motion.div>
+      <motion.div className="menu__link-wrapper" variants={linkVariants}>
+        <Link className="menu__link text--menu-link" to="/ilgiorno">
+          Il Giorno
+        </Link>
+      </motion.div>
     </motion.div>
   )
 }
