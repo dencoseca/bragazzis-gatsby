2fc476b0a08ea0b954c9f9a1d22385badea76c77
Leon Brown <leon.brown71@gmail.com>
2021-04-11 17:23:33 +0100

    animate menu button lines

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,50 @@
 import React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { motion, useAnimation } from "framer-motion"
+
+// Animations
+const smoothTransition = {
+  duration: 0.6,
+  ease: [0.43, 0.13, 0.13, 0.96],
+}
+
+const topLineVariants = {
+  closed: {
+    rotate: "0deg",
+    translateY: 0,
+  },
+  open: {
+    rotate: "45deg",
+    translateY: 6,
+  },
+}
+
+const bottomLineVariants = {
+  closed: {
+    rotate: "0deg",
+    translateY: 0,
+  },
+  open: {
+    rotate: "-45deg",
+    translateY: -5,
+  },
+}
 
 function Header({ location, menuIsOpen, setMenuIsOpen }) {
   const headerClassName =
     location.pathname === "/lastoria" ? "header header--dark" : "header"
 
+  const controls = useAnimation()
+
   function toggleMenu() {
     if (menuIsOpen) {
       setMenuIsOpen(false)
+      controls.start("closed")
     }
     if (!menuIsOpen) {
       setMenuIsOpen(true)
+      controls.start("open")
     }
   }
 
@@ -46,8 +79,20 @@ function Header({ location, menuIsOpen, setMenuIsOpen }) {
         </Link>
       </nav>
       <div className="header__mobile-menu-button" onClick={() => toggleMenu()}>
-        <div className="line"></div>
-        <div className="line"></div>
+        <motion.div
+          className="line"
+          initial="closed"
+          animate={controls}
+          variants={topLineVariants}
+          transition={smoothTransition}
+        ></motion.div>
+        <motion.div
+          className="line"
+          initial="closed"
+          animate={controls}
+          variants={bottomLineVariants}
+          transition={smoothTransition}
+        ></motion.div>
       </div>
     </div>
   )
